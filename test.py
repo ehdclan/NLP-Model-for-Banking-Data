0de1c8eb814c3f00e187b5ff16c3a7d1833425a2
ehdclan <ebukaharry66@gmail.com>
2025-11-18 10:41:36 +0100

    updated user input

--- test.py
+++ test.py
@@ -2,6 +2,26 @@ import pickle
 
 vectorizer, model = pickle.load(open('bank_model.pkl', 'rb'))
 
-text = ["I lost my card"]
-X = vectorizer.transform(text)
-print(model.predict(X))
+conversation_history = []
+first_interaction = True
+
+while True:
+    if first_interaction:
+        user_input = [input("Ask a bank related question: ").strip()]
+        first_interaction = False
+    else:
+        user_input = [input("\nAny other queries? (Type 'exit' to quit): ").strip()]
+
+    if user_input[0].lower() == "exit":
+        print("Thank you for banking with us.")
+        break
+    if not user_input:
+        print("Please enter a question: ")
+        continue
+
+    conversation_history.append(user_input)
+    
+    recent_convo = conversation_history[-3:] if len(conversation_history) > 3 else conversation_history
+
+    X = vectorizer.transform(user_input)
+    print(model.predict(X))
